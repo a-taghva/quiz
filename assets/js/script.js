1376e06e94c1f1958ee0dea3dcd3ebe54dddbc88
Ali Taghva <taghvali99@gmail.com>
2020-12-29 21:34:00 -0500

    add timer,  result section and enhance code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,32 @@
 let body = document.body;
 let mainEl = document.createElement("main");
 
-let questionCounter = 0;
-
+// * INFO ELEMENTS
 let infoEl = document.createElement("div");
 let highScoreEl = document.createElement("a");
 let timerEl = document.createElement("p");
 let timeLeftEl = document.createElement("span");
 
+// * INTRO ELEMENTS
 let introEl = document.createElement("section");
 let h1El = document.createElement("h1");
 let descriptionEl = document.createElement("div");
 let p1El = document.createElement("p");
 let p2El = document.createElement("p");
 let buttonEl = document.createElement("button");
+
+// * RESULT ELEMENTS
+let resultEl = document.createElement("section");
+let h2El = document.createElement("h2");
+let pEl = document.createElement("p");
+let scoreEl = document.createElement("span");
+let containerEl = document.createElement("div");
+let labelEl = document.createElement("label");
+let inputFieldEl = document.createElement("div");
+let inputEl = document.createElement("input");
+let submitEl = document.createElement("button");
+
+let questionCounter = 0;
 
 let quiz = [
     {
@@ -45,46 +58,90 @@ let quiz = [
 
 
 // create info div which contains highScore and timer
-infoEl.className = "info";
+let createInfoSection = function() {
+    infoEl.className = "info";
 
-highScoreEl.setAttribute("href", "#");
-highScoreEl.textContent = "View High Score";
+    highScoreEl.setAttribute("href", "#");
+    highScoreEl.textContent = "View High Score";
 
-timerEl.className = "timer";
-timerEl.textContent = "Time: ";
-timeLeftEl.className = "time-left";
-timeLeftEl.textContent = "0";
+    timerEl.className = "timer";
+    timerEl.textContent = "Time: ";
+    timeLeftEl.className = "time-left";
+    timeLeftEl.textContent = "0";
 
-timerEl.appendChild(timeLeftEl);
+    timerEl.appendChild(timeLeftEl);
 
-infoEl.appendChild(highScoreEl);
-infoEl.appendChild(timerEl);
+    infoEl.appendChild(highScoreEl);
+    infoEl.appendChild(timerEl);
+
+    body.appendChild(infoEl);
+}
+
 
 // create intro section
-introEl.className = "intro";
+let createIntroSection = function() {
+    introEl.className = "intro";
 
-h1El.textContent = "Coding Quiz Challenge";
+    h1El.textContent = "Coding Quiz Challenge";
 
-descriptionEl.className = "description";
-p1El.textContent = "Try to answer the following code-related questions within the time limit.";
-p2El.textContent = "Keep in mind that incorret answers will penalize your score/time by ten seconds!";
-descriptionEl.appendChild(p1El);
-descriptionEl.appendChild(p2El);
+    descriptionEl.className = "description";
+    p1El.textContent = "Try to answer the following code-related questions within the time limit.";
+    p2El.textContent = "Keep in mind that incorret answers will penalize your score/time by ten seconds!";
+    descriptionEl.appendChild(p1El);
+    descriptionEl.appendChild(p2El);
 
-buttonEl.className = "btn start-btn";
-buttonEl.innerText = "Start Quiz";
+    buttonEl.className = "btn start-btn";
+    buttonEl.innerText = "Start Quiz";
 
-introEl.appendChild(h1El);
-introEl.appendChild(descriptionEl);
-introEl.appendChild(buttonEl);
+    introEl.appendChild(h1El);
+    introEl.appendChild(descriptionEl);
+    introEl.appendChild(buttonEl);
 
-// add elements to page
-mainEl.appendChild(introEl)
+    mainEl.appendChild(introEl)
 
-body.appendChild(infoEl);
-body.appendChild(mainEl);
+    body.appendChild(mainEl);
+}
+
+// create result section
+let createResultSection = function(score) {
+    resultEl.className = "result";
+
+    h2El.textContent = "All Done!";
+    resultEl.appendChild(h2El);
+
+    pEl.textContent = "Your final score is ";
+    scoreEl.textContent = score + ".";
+    pEl.appendChild(scoreEl);
+
+    resultEl.appendChild(pEl);
+
+    // create container div
+    containerEl.className = "container";
+
+    labelEl.setAttribute("for", "name");
+    labelEl.textContent = "Enter initials: ";
+
+    containerEl.appendChild(labelEl);
+
+    inputFieldEl.className = "input-field";
+    inputEl.setAttribute("type", "text");
+    inputEl.setAttribute("id", "name");
+    submitEl.textContent = "Submit";
+    submitEl.setAttribute("type", "submit");
+    submitEl.className = "btn submit-btn";
+    
+    inputFieldEl.appendChild(inputEl);
+    inputFieldEl.appendChild(submitEl);
+
+    containerEl.appendChild(inputFieldEl);
 
 
+    resultEl.appendChild(containerEl);
+    console.log(resultEl);
+
+    mainEl.appendChild(resultEl);
+    body.appendChild(mainEl);
+}
 
 
 let createQuery = function(obj) {
@@ -122,6 +179,18 @@ let createChoices = function(obj) {
 }
 
 
+// ***** Timer ***** 
+let timer = function() {
+    timeLeftEl.innerText--;
+}
+
+let timeInterval;
+
+let startTimer = function (stop) {
+    timeLeftEl.innerText = 75;
+    timeInterval = setInterval(timer, 1000)
+}
+
 
 let getAnswer = function(event) {
     if (event.target.closest("li")) {
@@ -135,6 +204,7 @@ let checkAnswer = function(userAnswer, answer) {
         console.log("True");
     } else {
         console.log("False");
+        timeLeftEl.innerText -= 10;
     }
 
     questionCounter++;
@@ -142,14 +212,23 @@ let checkAnswer = function(userAnswer, answer) {
 }
 
 
+
 let mainFunction = function() {
+
     mainEl.innerHTML = "";
-    if (questionCounter !== quiz.length) {
-        mainEl.appendChild( createQuery(quiz[questionCounter]) );  
+    if (questionCounter === quiz.length) {
+        clearInterval(timeInterval);
+        createResultSection(timeLeftEl.innerText);
+    } else {
+        mainEl.appendChild( createQuery(quiz[questionCounter]) );
     }
 };
 
 
-buttonEl.addEventListener("click", mainFunction)
+createInfoSection();
+createIntroSection();
+
+buttonEl.addEventListener("click", mainFunction);
+buttonEl.addEventListener("click", startTimer);
 mainEl.addEventListener("click", getAnswer); 
 
